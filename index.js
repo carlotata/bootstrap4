42d3cab1fca1042a3c06ec3f709ab6e08dc9d51c
carlotata <avisojohn040@gmail.com>
2024-05-02 14:33:37 +0800

    changed something

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ function Note(){
 
     let notelist = `<div class="card mb-1"'><div class="card-body"><h4 class="card-title">${title1}</h4><hr><br><h6 class="card-subtitle mb-1">${note1}</h6><br><hr><p class="card-text">${date1}</p><button type="button" class="btn btn-danger" onclick="deleteNote(this)">DELETE</button></div></div>`;
     document.getElementById('noteList').innerHTML += notelist;
-    clearForm();
+
+    document.getElementById('title').value = '';
+    document.getElementById('note').value = '';
+    document.getElementById('date').value = '';
 }
 
 function deleteNote(button){
     button.closest('.card').remove();
-}
-
-function clearForm(){
-    document.getElementById('title').value = '';
-    document.getElementById('note').value = '';
-    document.getElementById('date').value = '';
 }
 
